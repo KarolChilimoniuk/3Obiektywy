f2137751918089e654ff92505b07842cd486f6e6
Karol Chilimoniuk <webdevkarolchilimoniuk@gmail.com>
2019-11-27 14:11:09 +0100

    feat: added loading div, animations and script for it

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,23 @@
 
 const mobileNav = document.querySelector(".navigation--js");
 const burgerIcon = document.querySelector(".menu__switcher--js")
+const loaderContainer = document.querySelector(".loader");
+const header = document.querySelector(".header")
+const introduction = document.querySelector(".introduction--js")
 ''
 burgerIcon.addEventListener("click", () => {
   burgerIcon.classList.toggle("no__active");
   mobileNav.classList.toggle("mobileMove");
 });
+
+(function loadingElements() {
+  setTimeout(() => {
+    loaderContainer.classList.add("loader--hidden");
+    header.classList.add("header--visible");
+    burgerIcon.classList.add("menu__switcher--visible")
+  }, 2000);
+
+  setTimeout(() => {
+    introduction.classList.add("introduction--visible");
+  }, 1500);
+}());
