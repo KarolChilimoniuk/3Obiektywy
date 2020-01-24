ab580d0ac70a5cc30a935d58acf1d07f843bd02a
KarolChilimoniuk <webdevkarolchilimoniuk@gmail.com>
2020-01-24 19:46:53 +0100

    feat: added Karol's instagram photos

--- src/js/karolSubPage.js
+++ src/js/karolSubPage.js
@@ -1,11 +1,10 @@
 "use strict";
 
-const header = document.querySelector(".header-karol--js");
 const nav = document.querySelector(".navigation--js");
 const burgerIcon = document.querySelector(".menu__switcher--js");
 const photographers = document.querySelector(".navigation__item--js");
 const submenu = document.querySelector(".submenu--js");
-const photoSection = document.querySelector(".section--photography--js");
+const image = document.querySelector(".section--photography__img--js");
 
 function loadingElements() {
   burgerIcon.addEventListener("click", () => {
@@ -17,14 +16,9 @@ function loadingElements() {
     submenu.classList.toggle("submenu--visible");
   });
 
-  // setTimeout(() => {
-  //   header.classList.add("header-karol--visible");
-  // }, 10);
-
-  setTimeout(() => {
-    photoSection.classList.add("section--photography--visible");
-    burgerIcon.classList.add("menu__switcher--visible");
-  }, 13);
+  image.addEventListener("click", () => {
+    image.classList.toggle("section--photography__img--bigger");
+  });
 }
 
 loadingElements();
