4b1950b1c61e46ee4da05fd2f8595f69d3767bbb
KarolChilimoniuk <webdevkarolchilimoniuk@gmail.com>
2020-01-25 14:07:15 +0100

    feat: added changing icons background in author section

--- src/js/karolSubPage.js
+++ src/js/karolSubPage.js
@@ -4,6 +4,7 @@ const nav = document.querySelector(".navigation--js");
 const burgerIcon = document.querySelector(".menu__switcher--js");
 const photographers = document.querySelector(".navigation__item--js");
 const submenu = document.querySelector(".submenu--js");
+const iconsContainer = document.querySelector(".header-author__icons--js");
 
 function loadingElements() {
   burgerIcon.addEventListener("click", () => {
@@ -13,6 +14,14 @@ function loadingElements() {
 
   photographers.addEventListener("click", () => {
     submenu.classList.toggle("submenu--visible");
+  });
+
+  window.addEventListener("scroll", function() {
+    if (window.pageYOffset >= 100) {
+      iconsContainer.classList.add("header-author__icons--bcg");
+    } else {
+      iconsContainer.classList.remove("header-author__icons--bcg");
+    }
   });
 }
 
